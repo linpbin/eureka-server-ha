9b2a38d57ab03e4feb3c432bb317c59400fa08e5
linpbin <linpbin@dingtalk.com>
2020-07-07 18:55:34 +0800

    feat(*): 增加build脚本

--- start.sh
+++ start.sh
@@ -4,9 +4,15 @@ cd $(dirname $0)
 img_mvn="maven:3.3.9-jdk-8"                               # docker image of maven
 m2_cache=~/.m2                                            # the local maven cache dir
 proj_home=$PWD
+img_output=eureka-server-ha
 
 docker run --rm \
   -v $m2_cache:/root/.m2 \
   -v $proj_home:/usr/src/mymaven \
   -w /usr/src/mymaven $img_mvn mvn clean package -U -DskipTests
 
+docker build -t $img_output .
+
+docker-compose down
+
+docker-compose up -d
